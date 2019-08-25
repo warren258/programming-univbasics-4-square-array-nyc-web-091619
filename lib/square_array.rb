8d80283059a58f5f53ce22313b1377c5327b3e48
Warren McDonald <warrenjmac@mail.com>
2019-08-25 17:03:56 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   #numbers = [1,2,3]
   results = []
   while i < numbers.length do
-    results << numbers[i] **2
+    results.push(numbers[i] **2)
     i  += 1
   end
   return results
